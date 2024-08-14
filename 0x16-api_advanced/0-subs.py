1753321ba2564fc22cc4e0ad9ede47ebdc4c24c9
ziadhuss3d <ziadhuss3d@example.com>
2024-08-14 06:03:46 -0700

    d

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,12 @@ import requests
 def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
-    
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
