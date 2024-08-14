36e90cdf96e53f728119ce32deec1abc6e489753
ziadhuss3d <ziadhuss3d@example.com>
2024-08-14 05:57:42 -0700

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Get Reddit Subscribers """
+import requests
+
+def number_of_subscribers(subreddit):
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
+    
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
+
+# Example usage
+if __name__ == "__main__":
+    subreddit = "python"
+    print(f"Subscribers: {number_of_subscribers(subreddit)}")
